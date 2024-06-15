d773feacad1f340655f119cd7a2584e770d3fcec
Akillot <nick29zozulya29@gmail.com>
2024-06-15 22:01:13 +0200

    Minor change.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(" ");
+        System.out.println();
 
             while(true) {
                 try {
