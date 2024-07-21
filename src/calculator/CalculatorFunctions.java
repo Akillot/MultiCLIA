4115874c9a5fbc0b03be4ab25764dfb50008710e
Akillot <nick29zozulya29@gmail.com>
2024-07-22 01:50:38 +0200

    Minor Changes.
    *The app is currently in process of re-building*
    *So please DO NOT use the current version of the app*

--- src/calculator/CalculatorFunctions.java
+++ src/calculator/CalculatorFunctions.java
@@ -14,7 +14,7 @@ public class CalculatorFunctions {
                 double numDouble = Double.parseDouble(num);
                 result += numDouble;
             } catch (NumberFormatException e) {
-                displayColorfulCommands(num + " is not a number", "red");
+                displayColorCommand(num + " is not a number", "red", (byte) 0);
                 return;
             }
         }
@@ -27,14 +27,14 @@ public class CalculatorFunctions {
         if (nums.length == 0) {
 
             drawHorizontalBorder(numberOfSymbols);
-            displayColorfulCommands("No numbers to subtract", "red");
+            displayColorCommand("No numbers to subtract", "red", (byte) 0);
             return;
         }
         result = 0;
         try {
             result = Double.parseDouble(nums[0]);
         } catch (NumberFormatException e) {
-            displayColorfulCommands(nums[0] + " is not a number", "red");
+            displayColorCommand(nums[0] + " is not a number", "red", (byte) 0);
             return;
         }
         for (int i = 1; i < nums.length; i++) {
@@ -42,7 +42,7 @@ public class CalculatorFunctions {
                 double numDouble = Double.parseDouble(nums[i]);
                 result -= numDouble;
             } catch (NumberFormatException e) {
-                displayColorfulCommands(nums[i] + " is not a number", "red");
+                displayColorCommand(nums[i] + " is not a number", "red", (byte) 0);
             }
         }
         showAnswer(result);
@@ -52,7 +52,7 @@ public class CalculatorFunctions {
     public static void multi(String mathStatement) {
         String[] nums = mathStatement.trim().split("\\s+");
         if (nums.length == 0) {
-            displayColorfulCommands("No numbers to multiply", "red");
+            displayColorCommand("No numbers to multiply", "red", (byte) 0);
             return;
         }
         result = 1;
@@ -61,7 +61,7 @@ public class CalculatorFunctions {
                 double numDouble = Double.parseDouble(num);
                 result *= numDouble;
             } catch (NumberFormatException e) {
-                displayColorfulCommands(num + " is not a number", "red");
+                displayColorCommand(num + " is not a number", "red", (byte) 0);
                 return;
             }
         }
@@ -72,14 +72,14 @@ public class CalculatorFunctions {
     public static void divide(String mathStatement) {
         String[] nums = mathStatement.trim().split("\\s+");
         if (nums.length == 0) {
-            displayColorfulCommands("No numbers to divide", "red");
+            displayColorCommand("No numbers to divide", "red", (byte) 0);
             return;
         }
         result = 1;
         try {
             result = Double.parseDouble(nums[0]);
         } catch (NumberFormatException e) {
-            displayColorfulCommands(nums[0] + " is not a number", "red");
+            displayColorCommand(nums[0] + " is not a number", "red", (byte) 0);
             return;
         }
 
@@ -87,12 +87,12 @@ public class CalculatorFunctions {
             try {
                 double numDouble = Double.parseDouble(nums[i]);
                 if (numDouble == 0) {
-                    displayColorfulCommands("Division by zero detected.\nAborting operation", "red");
+                    displayColorCommand("Division by zero detected.\nAborting operation", "red", (byte) 0);
                     return;
                 }
                 result /= numDouble;
             } catch (NumberFormatException e) {
-                displayColorfulCommands(nums[i] + " is not a number", "red");
+                displayColorCommand(nums[i] + " is not a number", "red", (byte) 0);
                 return;
             }
         }
@@ -103,14 +103,14 @@ public class CalculatorFunctions {
     public static void pow(String mathStatement) {
         String[] nums = mathStatement.trim().split("\\s+");
         if (nums.length == 0) {
-            displayColorfulCommands("No numbers to process", "red");
+            displayColorCommand("No numbers to process", "red", (byte) 0);
             return;
         }
         result = 1;
         try {
             result = Double.parseDouble(nums[0]);
         } catch (NumberFormatException e) {
-            displayColorfulCommands(nums[0] + " is not a number", "red");
+            displayColorCommand(nums[0] + " is not a number", "red", (byte) 0);
             return;
         }
         for (int i = 1; i < nums.length; i++) {
@@ -118,7 +118,7 @@ public class CalculatorFunctions {
                 double numDouble = Double.parseDouble(nums[i]);
                 result = Math.pow(result, numDouble);
             } catch (NumberFormatException e) {
-                displayColorfulCommands(nums[i] + " is not a number", "red");
+                displayColorCommand(nums[i] + " is not a number", "red", (byte) 0);
                 return;
             }
         }
