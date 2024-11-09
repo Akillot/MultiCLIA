b341ba139e6e322800785dc2a96cb49fdd4a900c
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2024-11-09 06:28:41 +0100

    Minor Update
    Working on Reminder

--- src/main/java/extensions/time/Reminder.java
+++ src/main/java/extensions/time/Reminder.java
@@ -1,14 +1,21 @@
 package extensions.time;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 import static core.logic.ColorFunc.*;
 import static core.logic.DisplayManager.messageModifier;
 import static core.logic.TextFunc.alignment;
 import static java.lang.System.out;
 
 public class Reminder {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
     public static void reminderStarted(){
+
         messageModifier('n',2);
         out.print(alignment(58) + WHITE + BOLD + "Set date and time to remind: " + RESET);
-
     }
 }
