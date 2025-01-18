43d1e7736c2908542827032ff416000c68d1827f
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-01-18 23:20:19 +0100

    Main color has been changed

--- src/main/java/core/configs/AppearanceConfigs.java
+++ src/main/java/core/configs/AppearanceConfigs.java
@@ -14,7 +14,7 @@ import static java.lang.System.out;
 public class AppearanceConfigs {
 
     //Colors
-    public static int systemMainColor = 247;
+    public static int systemMainColor = 147;
     public static int systemLayoutColor = 15;
 
     public static int systemAcceptanceColor = 46;
