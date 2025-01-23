10af51f157c734233f14867c5a21d68130f33f16
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-01-23 23:51:44 +0100

    Support page has been added to the project

---
+++ src/main/java/core/pages/SupportPage.java
@@ -0,0 +1,12 @@
+package core.pages;
+
+public class SupportPage {
+
+    public static void displaySupportPage() {
+        displaySupportMenu();
+    }
+
+    private static void displaySupportMenu() {
+
+    }
+}
