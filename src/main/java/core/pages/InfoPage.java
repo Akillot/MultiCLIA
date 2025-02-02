9331f72db859e1c72f1c584d1b1d483d9dc98a28
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-02-02 22:59:46 +0100

    Network page has been updated

--- src/main/java/core/pages/InfoPage.java
+++ src/main/java/core/pages/InfoPage.java
@@ -14,7 +14,7 @@ public class InfoPage {
 
     @Contract(pure = true)
     public static @NotNull String getVersion() {
-        String appVersion = "A-0.8.7";
+        String appVersion = "A-0.9.0";
         return getColorText(appVersion, sysMainColor);
     }
 
