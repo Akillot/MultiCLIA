59978cf4d3760ab05570191d8c03f619095bf252
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-02-22 23:05:07 +0100

    Little changes

--- src/main/java/core/ui/pages/time/CalendarPage.java
+++ src/main/java/core/ui/pages/time/CalendarPage.java
@@ -7,10 +7,61 @@ import static core.configs.AppearanceConfigs.*;
 import static core.configs.TextConfigs.*;
 
 import static core.configs.TextConfigs.alignment;
+import static core.logic.CommandManager.exitPage;
+import static core.logic.CommandManager.mainMenuRerun;
+import static core.ui.DisplayManager.clearTerminal;
+import static core.ui.DisplayManager.scanner;
 import static java.lang.System.out;
 
 public class CalendarPage {
+
     public static void displayCalendarPage() {
+        marginBorder(1, 2);
+        message("AI:", sysLayoutColor, getDefaultTextAlignment(), getDefaultDelay(), out::print);
+
+        displayListOfCommands();
+
+        while (true) {
+            slowMotionText(getDefaultDelay(), getSearchingLineAlignment(), false,
+                    getColor(sysLayoutColor) + searchingArrow, "");
+            String input = scanner.nextLine().toLowerCase();
+
+            switch (input) {
+                case "ask chatgpt", "/ac" -> displayCalendar();
+                case "rerun", "/rr" -> {
+                    insertControlChars('n', 1);
+                    mainMenuRerun();
+                }
+                case "clear terminal", "/cl" -> clearTerminal();
+                case "list of commands", "/lc" -> displayListOfCommands();
+                case "exit", "/e" -> {
+                    exitPage();
+                    return;
+                }
+                default -> out.print("");
+            }
+        }
+    }
+
+    public static void displayListOfCommands()  {
+        insertControlChars('n', 1);
+        message("·  Calendar [" + getColor(sysMainColor)
+                + "/ca" + getColor(sysLayoutColor) + "]", sysLayoutColor, getDefaultTextAlignment(), getDefaultDelay(), out::print);
+
+        message("·  Rerun [" + getColor(sysMainColor)
+                + "/rr" + getColor(sysLayoutColor) + "]", sysLayoutColor, getDefaultTextAlignment(), getDefaultDelay(), out::print);
+
+        message("·  Clear terminal [" + getColor(sysMainColor)
+                + "/cl" + getColor(sysLayoutColor) + "]", sysLayoutColor, getDefaultTextAlignment(), getDefaultDelay(), out::print);
+
+        message("·  List Of Commands [" + getColor(sysMainColor)
+                + "/lc" + getColor(sysLayoutColor) + "]", sysLayoutColor, getDefaultTextAlignment(), getDefaultDelay(), out::print);
+
+        message("·  Exit [" + getColor(sysMainColor)
+                + "/e" + getColor(sysLayoutColor) + "]", sysLayoutColor, getDefaultTextAlignment(), getDefaultDelay(), out::println);
+    }
+
+    private static void displayCalendar() {
         YearMonth month = YearMonth.now();
 
         marginBorder(1,2);
